70f18587d87acdbd64e494f61300e19b3d0b849c
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-05-31 14:49:39 +0530

    changes

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -3,12 +3,12 @@ const ErrorHandler = require("../utils/errorHandler");
 const transactionModel = require("../models/transactionModel");
 
 exports.getMyTransaction = catchAsyncError(async (req, res, next) => {
-    
-  const transactions = await transactionModel.find({user:req.userId}).lean();
+  const transactions = await transactionModel.find({ user: req.userId }).lean();
   let subscription = {};
   if (transactions.length) {
-    subscription.amount = transactions[transactions.length-1].amount;
-    subscription.validity = transactions[transactions.length-1].validity;
+    if (new Date(transactions[transactions.length - 1].expiry) > new Date())
+      subscription.amount = transactions[transactions.length - 1].amount;
+    subscription.validity = transactions[transactions.length - 1].validity;
   }
   res.status(200).json({
     success: true,
