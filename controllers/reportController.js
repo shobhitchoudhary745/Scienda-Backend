a2395f0032c160fa504ef20aefb4d0d2f2b0abc3
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-06-03 15:36:37 +0530

    changes

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -3,38 +3,46 @@ const ErrorHandler = require("../utils/errorHandler");
 const reportModel = require("../models/reportModel");
 
 exports.getReports = catchAsyncError(async (req, res, next) => {
-    const { subdomain_reference, key } = req.query;
-    const query = {};
-    if (subdomain_reference) {
-      query.subdomain_reference = subdomain_reference;
-    }
-    if (key) {
-      if (key) query.test_name = { $regex: new RegExp(key, "i") };
-    }
-    const tests = await testModel
-      .find(query)
-      .populate({
-        path: "questions_reference",
-        select: "-correct_option",
+  const { subdomain_reference, key } = req.query;
+  const query = {};
+  if (subdomain_reference) {
+    query.subdomain_reference = subdomain_reference;
+  }
+  if (key) {
+    if (key) query.test_name = { $regex: new RegExp(key, "i") };
+  }
+  const tests = await testModel
+    .find(query)
+    .populate({
+      path: "questions_reference",
+      select: "-correct_option",
+      populate: {
+        path: "sub_topic_reference",
+
         populate: {
-          path: "sub_topic_reference",
-  
-          populate: {
-            path: "topic_reference",
-          },
+          path: "topic_reference",
         },
-      })
-      .populate("subdomain_reference")
-      .lean();
-    res.status(200).json({
-      success: true,
-      tests,
-      message: "Tests fetch Successfully",
-    });
+      },
+    })
+    .populate("subdomain_reference")
+    .lean();
+  res.status(200).json({
+    success: true,
+    tests,
+    message: "Tests fetch Successfully",
   });
+});
 
 exports.getReport = catchAsyncError(async (req, res, next) => {
-  const report = await reportModel.findById(req.params.id).populate("test");
+  const report = await reportModel
+    .findById(req.params.id)
+    .populate({
+      path: "test",
+      populate: {
+        path: "questions_reference",
+      },
+    })
+    .lean();
   if (!report) return next(new ErrorHandler("Report not found", 404));
   res.status(200).json({
     success: true,
