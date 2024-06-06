b4af7a022354b7514acb7359b37d3ab7fff13f59
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-06-06 15:44:58 +0530

    changes

--- routes/planRoutes.js
+++ routes/planRoutes.js
@@ -5,13 +5,13 @@ const {
   deletePlan,
   updatePlan,
 } = require("../controllers/planController");
-const { auth, isAdmin } = require("../middlewares/auth");
+const { auth, isAdmin, isNotUser } = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.post("/create-plan", auth, isAdmin, createPlan);
 router.get("/get-plans", getPlans);
-router.delete("/delete-plan/:id", auth, isAdmin, deletePlan);
-router.patch("/update-plan/:id", auth, isAdmin, updatePlan);
+router.delete("/delete-plan/:id", auth, isNotUser, deletePlan);
+router.patch("/update-plan/:id", auth, isNotUser, updatePlan);
 
 module.exports = router;
