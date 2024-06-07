bd0fdb5c0ccd6502ce2c433c0e1a99e23ed65bb7
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-06-07 14:26:21 +0530

    changes

--- controllers/salaryController.js
+++ controllers/salaryController.js
@@ -3,9 +3,39 @@ const catchAsyncError = require("../utils/catchAsyncError");
 const ErrorHandler = require("../utils/errorHandler");
 
 exports.getSalaries = catchAsyncError(async (req, res, next) => {
+  const { from, to } = req.query;
+
+  const obj = {};
+  if (from) {
+    const [day, month, year] = from.split("-").map(Number);
+    const fromdate = new Date(year, month - 1, day+1);
+    obj.createdAt = {
+      $gte: fromdate,
+    };
+  }
+  if (to) {
+    const [day, month, year] = to.split("-").map(Number);
+    const todate = new Date(year, month - 1, day+1);
+    obj.createdAt = {
+      $lte: todate,
+    };
+  }
+
+  if (from && to) {
+    const [day, month, year] = from.split("-").map(Number);
+
+    const [day1, month1, year1] = to.split("-").map(Number);
+    const fromdate = new Date(year, month - 1, day+1);
+    const todate = new Date(year1, month1 - 1, day1+1);
+
+    obj.createdAt = {
+      $gte: fromdate,
+      $lte: todate,
+    };
+  }
   const salarys = await salaryModel
-    .find({ professor: req.userId })
-    .populate({ path: "area_wise.subdomain",select:["sub_domain_name"] })
+    .find({ professor: req.userId, ...obj })
+    .populate({ path: "area_wise.subdomain", select: ["sub_domain_name"] })
     .lean();
 
   res.status(200).send({
