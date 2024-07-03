e09ebf152acccc0e26bcf0619a332ec149666c7a
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-07-03 15:27:29 +0530

    changes

--- controllers/planController.js
+++ controllers/planController.js
@@ -1,9 +1,10 @@
 const planModel = require("../models/planModel");
+const subDomainModel = require("../models/subDomainModel");
 const catchAsyncError = require("../utils/catchAsyncError");
 const ErrorHandler = require("../utils/errorHandler");
 
 exports.createPlan = catchAsyncError(async (req, res, next) => {
-  const { price, validity, features } = req.body;
+  const { price, validity, features, subdomain } = req.body;
   if (!price || !validity) {
     return next(new ErrorHandler("All Fieleds are required", 400));
   }
@@ -14,6 +15,12 @@ exports.createPlan = catchAsyncError(async (req, res, next) => {
   });
 
   await plan.save();
+
+  if (subdomain) {
+    const subdomains = await subDomainModel.findById(subdomain);
+    subdomains.plans = subdomains.plans.push(plan._id);
+    await subdomains.save();
+  }
 
   res.status(200).json({
     success: true,
