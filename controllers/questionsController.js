4e9c7e89779ad01455ff7d9b1159269f1da941ca
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-07-16 10:43:46 +0530

    changes

--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -116,7 +116,7 @@ exports.getQuestions = catchAsyncError(async (req, res, next) => {
   } else if (req.query.topic) {
     questions = questions.filter(
       (question) =>
-        question.sub_topic_reference.topic_reference._id.toString() ==
+        question.sub_topic_reference?.topic_reference?._id?.toString() ==
         req.query.topic
     );
   }
